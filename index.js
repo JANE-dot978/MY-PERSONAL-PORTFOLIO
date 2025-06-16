d1832bcf35e2418dbd562b3f306a2c6092fc45d3
JANE-dot978 <gathujane97@gmail.com>
2025-06-16 21:55:58 +0300

    added animted gradient background and adjusted footer to a full span of full width ofthe page

--- index.js
+++ index.js
@@ -47,3 +47,11 @@ document.addEventListener('DOMContentLoaded', () => {
   popouts.forEach(el => observer.observe(el));
 });
 
+
+  const colors = ["#ff9a9e", "#fad0c4", "#fbc2eb", "#a18cd1", "#b2fefa", "#f6d365"];
+  setInterval(() => {
+    document.body.style.background = colors[Math.floor(Math.random() * colors.length)];
+  }, 3000);
+
+
+
