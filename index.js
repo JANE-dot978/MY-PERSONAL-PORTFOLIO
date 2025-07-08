47f6fe2b0f491d7a11fc874eb71ed3d5c7dd08b4
JANE-dot978 <gathujane97@gmail.com>
2025-07-08 20:41:11 +0300

    add responsive fixed top navbar with toggleable mobile menu

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+//navbar
+
+  const toggleBtn = document.getElementById('menu-toggle');
+  const navMenu = document.getElementById('nav-menu');
+
+  toggleBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('hidden');
+    navMenu.classList.toggle('flex');
+  });
+
+
 //this is for the contact form
 document.getElementById('contactForm').addEventListener('submit', function (e) {
   e.preventDefault();
@@ -31,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const observerOptions = {
     root: null,
     rootMargin: '0px',
-    threshold: 0.2, // Trigger when 20% of the element is visible
+    threshold: 0.3, // Trigger when 20% of the element is visible
   };
 
   const observer = new IntersectionObserver((entries) => {
@@ -39,13 +50,19 @@ document.addEventListener('DOMContentLoaded', () => {
       if(entry.isIntersecting){
         entry.target.classList.add('visible');
         // Optionally unobserve after animation to improve performance
-        observer.unobserve(entry.target);
+        // observer.unobserve(entry.target);
       }
     });
   }, observerOptions);
 
   popouts.forEach(el => observer.observe(el));
 });
+      if(entry.isIntersecting){
+        entry.target.classList.add('visible');
+      } else {
+        entry.target.classList.remove('visible');
+      }
+
 
 
   // const colors = ["#ff9a9e", "#fad0c4", "#fbc2eb", "#a18cd1", "#b2fefa", "#f6d365"];
